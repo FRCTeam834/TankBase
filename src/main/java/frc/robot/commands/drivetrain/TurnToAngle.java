37f0152939e8c009081a55be6b4d4e33085d7b08
mdurrani808 <mdurrani808@gmail.com>
2021-11-14 10:55:30 -0500

    added drive helper classes

--- src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
+++ src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
@@ -2,31 +2,45 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.drivetrain;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 
 public class TurnToAngle extends CommandBase {
+  PIDController anglePID = new PIDController(.5, 0, 0);
+  double targetAngle = 0.0;
+  double currentAngle = 0.0;
   /** Creates a new TurnToAngle. */
-  public TurnToAngle() {
+  public TurnToAngle(double targetAngle) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.navX,RobotContainer.driveTrain);
+    this.targetAngle = targetAngle;
+    anglePID.setTolerance(3);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    currentAngle = RobotContainer.navX.getYaw();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    RobotContainer.driveTrain.arcadeDrive(0, anglePID.calculate(RobotContainer.navX.getYaw(), currentAngle + targetAngle));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    RobotContainer.driveTrain.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return anglePID.atSetpoint();
   }
 }
